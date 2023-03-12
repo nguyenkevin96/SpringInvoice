5c9d28dccbd16828289648782c24ef75c3338413
Kevin Nguyen <kknguy24@gmail.com>
2023-03-12 12:55:40 -0500

    Added login functionality  - jwtToken stores in local storage  - created the login form (formBuilder)  - added Validators to login form  - added /login to skip when adding authorization to header

--- src/app/service/invoice.service.ts
+++ src/app/service/invoice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Part} from "../customer/customer-invoice/customer-parts/customer-parts.component";
+import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,10 @@ export class InvoiceService {
     baseUrl = environment.baseUrl;
 
     constructor(private http: HttpClient) {
+    }
 
+    checkLogin(data: any): Observable<any> {
+        return this.http.post(this.baseUrl + '/api/v1/auth/authenticate', data);
     }
 
     submitForm() {
